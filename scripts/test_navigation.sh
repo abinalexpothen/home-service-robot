303a751d279fe960ec091cd6a7fd90dd451bd994
abinalexpothen <abinalexpothen@gmail.com>
2021-11-05 20:16:05 -0400

    feat: create collision map, add slam map later.

--- scripts/test_navigation.sh
+++ scripts/test_navigation.sh
@@ -3,7 +3,7 @@
 cd "$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"/../
 
 world_file_location="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"/world/abinshomeoffice.world
-map_file_location="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"/world/abinshomeoffice.pgm
+map_file_location="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"/map/abinshomeoffice.yaml
 
 cd "$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"/../
 
@@ -11,6 +11,6 @@ setup_file_location="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"/deve
 
 xterm -e " source $setup_file_location; roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$world_file_location " &
 sleep 10
-xterm -e " source $setup_file_location; roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$map_file_location" &
+xterm -e " source $setup_file_location; roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$map_file_location initial_pose_a:=-1.571" &
 sleep 5
 xterm -e " source $setup_file_location; roslaunch turtlebot_rviz_launchers view_navigation.launch "
